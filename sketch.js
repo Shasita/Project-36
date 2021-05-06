265a034d96bd2f55ec6553fbed54cb87bb0e4841
Shasita <72453538+Shasita@users.noreply.github.com>
2021-05-06 11:56:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,124 @@
+var dog,sadDog,happyDog, database;
+var foodS,foodStock;
+var fedTime,lastFed;
+var feed,addFood;
+var foodObj;
+var gameState;
+var bedroom, garden, washroom;
+var currentTime;
+
+function preload(){
+  sadDog=loadImage("Dog.png");
+  happyDog=loadImage("happy dog.png");
+
+  bedroom = loadImage("Bed Room.png");
+  wahroom = loadImage("Wash Room.png");
+  garden = loadImage("Garden.png");
+
+}
+
+function setup() {
+  database=firebase.database();
+  createCanvas(1000,400);
+
+  foodObj = new Food();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+
+  
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+  
+  feed=createButton("Feed the dog");
+  feed.position(700,95);
+  feed.mousePressed(feedDog);
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
+}
+
+function draw() {
+  background(46,139,87);
+
+  readGameState();
+  text(gameState, 100, 300);
+
+  imageMode (CENTER);
+  if(gameState == 'hungry'){
+   foodObj.bedroom();
+  }
+currentTime = hour();
+
+  if(currentTime == lastFed + 1){
+    foodObj.garden();
+  }
+
+  if(currentTime > lastFed + 2 && currentTime < lastFed + 4){
+    foodObj.washroom();
+  }
+
+  foodObj.display();
+
+  fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed=data.val();
+  });
+ 
+  fill(255,255,254);
+
+  textSize(15);
+
+  if(lastFed>=12){
+    text("Last Feed : "+ lastFed%12 + " PM", 350,30);
+   }else if(lastFed==0){
+     text("Last Feed : 12 AM",350,30);
+   }else{
+     text("Last Feed : "+ lastFed + " AM", 350,30);
+   }
+ 
+  
+
+  drawSprites();
+}
+
+//function to read food Stock
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+//function to update food stock and last fed time
+function feedDog(){
+  dog.addImage(happyDog);
+  
+  if(foodObj.getFoodStock()<= 0){
+    foodObj.updateFoodStock(foodObj.getFoodStock()*0);
+  }else{
+    foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  }
+  
+  database.ref('/').update({
+    Food:foodObj.getFoodStock(),
+    FeedTime:hour()
+  })
+}
+
+//function to add food in stock
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
+
+function readGameState(){
+  database.ref('gameState').on("value", function (data){
+    gameState = data.val();
+  });
+}
